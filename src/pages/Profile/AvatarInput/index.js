4dd80c69001090604d783de37907cdcdbe0f5509
Jonas <jonaslagoteixeira@gmail.com>
2020-01-10 11:19:06 -0300

    feito estilizacao do input de avatar

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useField } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function AvatarInput() {
-  return <div />;
+  function handleChange(e) {}
+
+  return (
+    <Container>
+      <label htmlFor="avatar">
+        <img src="" alt="" />
+
+        <input
+          type="file"
+          id="avatar"
+          accept="image/*"
+          onChange={handleChange}
+        />
+      </label>
+    </Container>
+  );
 }
